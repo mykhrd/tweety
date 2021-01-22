c61d9290d0585191de2d3005329d7f007dda1e23
mykhrd <mykhrd@gmail.com>
2021-01-22 13:36:16 +0900

    add friends

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex">
-        <div class="flex-1">
+    <div class="lg:flex">
+        <div class="lg:w-1/4">
             @include('_sidebar-links')
         </div>
-        <div class="flex-1">2</div>
-        <div class="flex-1">3</div>
+        <div class="flex-1">
+
+        </div>
+        <div class="lg:w-1/4">
+            @include('_friends-list')
+        </div>
     </div>
 @endsection
