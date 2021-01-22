b22cec8d4134287ce8c7b4c1be14d8aeea71586f
mykhrd <mykhrd@gmail.com>
2021-01-22 13:20:47 +0900

    add sidebar links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    dashboard
+    <div class="flex">
+        <div class="flex-1">
+            @include('_sidebar-links')
+        </div>
+        <div class="flex-1">2</div>
+        <div class="flex-1">3</div>
+    </div>
 @endsection
