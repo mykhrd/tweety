d3f0a7da0c31b69839b14cb993f0b51d46597a88
mykhrd <mykhrd@gmail.com>
2021-01-22 22:11:05 +0900

    edit files

--- app/User.php
+++ app/User.php
@@ -42,11 +42,29 @@ class User extends Authenticatable
 
     public function timeline()
     {
-        return Tweet::where('user_id', $this->id)->latest()->get();
+        // include all user's tweets
+        // as well as tweets from users they follow
+        // in descending order by date
+
+    }
+
+    public function tweets()
+    {
+        return $this->hasMany(Tweet::class);
     }
 
     public function getAvatarAttribute()
     {
         return 'https://i.pravatar.cc/40?u=' . $this->email;
+    }
+
+    public function follow(User $user)
+    {
+        return $this->follows()->save($user);
+    }
+
+    public function follows()
+    {
+        return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
                 @endforeach
             </div>
         </div>
-        <div class="lg:w-1/6 bg-blue-50 rounded-lg p-4">
+        <div class="lg:w-1/5 bg-blue-50 rounded-lg p-4">
             @include('_friends-list')
         </div>
     </div>
