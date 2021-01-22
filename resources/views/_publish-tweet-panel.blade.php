52c1421e17e2c5cf00786f0a6aee8a8722bdfb63
mykhrd <mykhrd@gmail.com>
2021-01-22 14:11:23 +0900

    add tweet panel

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,18 @@
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
+    <form action="">
+        <textarea name="" class="w-full" placeholder="Hello"></textarea>
+    </form>
+    <hr class="my-4">
+
+    <footer class="flex justify-between">
+        <div class="flex items-center text-sm">
+            <img src="https://i.pravatar.cc/40" alt="" class="rounded-full mr-2">
+            John Doe
+        </div>
+
+        <button type="submit"
+                class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">
+            Tweet
+        </button>
+    </footer>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="lg:flex">
-        <div class="lg:w-1/4">
+    <div class="lg:flex lg:justify-between">
+        <div class="lg:w-32">
             @include('_sidebar-links')
         </div>
-        <div class="flex-1">
-
+        <div class="flex-1 lg:mx-10" style="max-width: 700px">
+            @include('_publish-tweet-panel')
+            <div class="border boder-gray-300 rounded-lg">
+                @include('_tweet')
+                @include('_tweet')
+                @include('_tweet')
+                @include('_tweet')
+            </div>
         </div>
-        <div class="lg:w-1/4">
+        <div class="lg:w-1/6 bg-blue-50 rounded-lg p-4">
             @include('_friends-list')
         </div>
     </div>
